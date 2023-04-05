62b9c349f150fd713d6bf7bce2321e507e7766d7
Kamdzik <127319303+Kamdzik@users.noreply.github.com>
2023-04-05 12:36:17 +0000

    wygląd

--- Main.java
+++ Main.java
@@ -15,12 +15,13 @@ class Main {
     
     for(int a = 4 ; a>=0;a--){
       System.out.println("");
-    
+  
       for(int b = 0 ; b<=a; b++){
        System.out.print("*");
       }
-
       
     }
+
+    
   }
 }
